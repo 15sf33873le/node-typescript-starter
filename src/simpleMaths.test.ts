f373b93951a87e0dccc2ec2219f39dfa75174f4d
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-18 09:35:57 +0000

    added a function to calculate compound interest; takes(starting sum, interest rate, time in number of intervals) and returns the end sum. Can take negative interest values, which reduces the sum by said interest. Will default time to 1

--- src/simpleMaths.test.ts
+++ src/simpleMaths.test.ts
@@ -1,4 +1,4 @@
-import { calcAreaSquare, calcAreaTriangle } from './simpleMaths'
+import { calcAreaSquare, calcAreaTriangle, calcCompoundInterest } from './simpleMaths'
 
 test('Area Square', () => {
     expect(calcAreaSquare(4)).toBe(16);
@@ -6,4 +6,28 @@ test('Area Square', () => {
 
 test('Area Triangle', () => {
     expect(calcAreaTriangle(4, 5)).toBe(10);
+})
+
+test("simple positive Compound Interest", () => {
+    expect(calcCompoundInterest(100, 0.05)).toBe(105)
+})
+
+test("small starting sum Compound Interest", () => {
+    expect(calcCompoundInterest(1, 0.05)).toBe(1.05)
+})
+
+test("large starting sum Compound Interest", () => {
+    expect(calcCompoundInterest(1000000000, 0.05)).toBe(1050000000)
+})
+
+test("multiple interval Compound Interest", () => {
+    expect(calcCompoundInterest(100, 0.05, 5)).toBe(100*(1.05**5))
+})
+
+test("negative Compound Interest", () => {
+    expect(calcCompoundInterest(100, -0.05)).toBe(95)
+})
+
+test("multiple interval negative Compound Interest", () => {
+    expect(calcCompoundInterest(100, -0.05, 5)).toBe(100*(0.95**5))
 })
--- src/simpleMaths.ts
+++ src/simpleMaths.ts
@@ -6,3 +6,7 @@ export function calcAreaTriangle(width: number, height: number): number {
     return width * height / 2;
 }
 
+export function calcCompoundInterest(startSum: number, interest: number, time = 1): number {
+    return startSum * Math.pow(1+interest, time)
+}
+
